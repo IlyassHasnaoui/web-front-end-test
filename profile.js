179e96980cc3bc2b8c291faa346dd3b4542d4207
“Ilyass <ilyass.has@live.fr>
2023-10-01 15:50:33 -0400

    profile.html final 2

---
+++ profile.js
@@ -0,0 +1,15 @@
+const colorButton = document.getElementById('color');
+const placeButton = document.getElementById('place');
+const ritualButton = document.getElementById('ritual');
+
+colorButton.addEventListener('click', function() {
+    alert("My favorite color is black.");
+});
+
+placeButton.addEventListener('click', function() {
+    alert("My favorite place is Morocco.");
+});
+
+ritualButton.addEventListener('click', function() {
+    alert("My favorite ritual is getting a quick workout in the morning.");
+});
