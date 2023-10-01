1322b44fb2198f0d1e8439caa5c223f50ce96653
“Ilyass <ilyass.has@live.fr>
2023-10-01 15:29:43 -0400

    about.js Final

--- about.js
+++ about.js
@@ -5,10 +5,16 @@ console.log("hello world");
 function handleSubmit(evt) {
 	evt.preventDefault();
 	
-	console.log('form submit');
+	alert('Form submitted successfuly');
 }
 
 
 let form = document.querySelector('#contact');
 
 form.addEventListener('submit', handleSubmit);
+
+const rubberDuck = document.querySelector('img');
+
+rubberDuck.addEventListener('mouseover', function() {
+	alert('Quack Quack!');
+})
